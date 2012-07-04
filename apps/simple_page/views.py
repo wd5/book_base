bcd0c540a80da04a1b92077f50b340d64df3e5af
DrMartiner <DrMartiner@GMail.Com>
2012-07-04 07:34:55 +0300

    Вывел списко последних книг на главную

--- apps/simple_page/views.py
+++ apps/simple_page/views.py
@@ -2,5 +2,12 @@
 
 from django.views.generic.base import TemplateView
 
+from apps.books.models import Book
+
 class MainPage(TemplateView):
     template_name = 'simple_page/main_page.html'
+
+    def get_context_data(self, **kwargs):
+        context=super(MainPage, self).get_context_data(**kwargs)
+        context['latest_book']=Book.objects.order_by('-update')
+        return context
